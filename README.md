c1e06ee0a2feabec54632b4642d58f6e999d9e36
Taffy Cheng <taffy128s@gmail.com>
2016-08-27 23:47:44 +0800

    Init

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # AutoDownloadMusic
+
+[Requirements]
+-Python 2.7
+-Selenium google chrome driver
+-Google Chrome
+
+[usage]
+-key some words in song_list
+(Samples in the song_list. Make sure you have a newline of the last song)
+
---
+++ auto_catch.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+
+import time, urllib2, sys
+import lxml.html as lh
+from selenium import webdriver
+
+song_list = []
+with open('song_list') as f:
+    song_list = f.readlines()
+
+for song in song_list:
+    googleDriver = webdriver.Chrome()
+    googleDriver.get('https://www.google.com#q=' + song)
+    link = ''
+    count = 0
+    while link is '':
+        try:
+            link = googleDriver.find_elements_by_tag_name('cite')[count].text
+            if ('youtube' in link) is False:
+                link = ''
+                count += 1
+        except:
+            pass
+    googleDriver.close()
+
+    converterDriver = webdriver.Chrome()
+    converterDriver.get('http://www.youtube-mp3.org/')
+    element = converterDriver.find_element_by_id('youtube-url')
+    element.clear()
+    element.send_keys(link)
+    converterDriver.find_element_by_id('submit').click()
+    # sleep
+    # time.sleep(5)
+    # sleep
+    down_link = ''
+    while down_link is '':
+        try:
+            down_link = converterDriver.find_element_by_id('dl_link').find_elements_by_tag_name('a')[2].get_attribute('href')
+        except:
+            pass
+    converterDriver.close()
+    print 'Downloading ' + song
+    while True:
+        try:
+            req = urllib2.Request(down_link, headers={'User-Agent' : 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/52.0.2743.116 Safari/537.36'})
+            mp3file = urllib2.urlopen(req)
+            with open(song[0 : len(song) - 1] + '.mp3','wb') as output:
+                output.write(mp3file.read())
+            break
+        except Exception as e:
+            print e
